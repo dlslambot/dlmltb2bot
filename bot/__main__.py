5bf7d298bd4bf7b0a8b83d470536ab6999b4f580
dlslambot <106543696+dlslambot@users.noreply.github.com>
2022-06-22 18:00:40 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,17 +65,17 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("⚡Owner⚡", "https://t.me/Dark_LordSl")
+    buttons.buildbutton("🧲OUR MIRROR-LEECH GROUP🧲", "https://t.me/+cvPJqSAyHuNmNzk1")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-This bot can mirror all your links to Google Drive!
+Hi, I'm a Mirror-Leech Bot which can Leech And Mirror all your Links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update.message, reply_markup)
+        sendMarkup('Oops! Not An Authorised User, You Can Use Me In The Official Mirror-Leech Group', context.bot, update.message, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update.message)
