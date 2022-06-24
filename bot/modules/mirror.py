2eec17d34cd2e4a96be9958ebde61d255286d159
dlslambot <106543696+dlslambot@users.noreply.github.com>
2022-06-24 12:34:10 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -200,7 +200,7 @@ class MirrorListener:
             msg += f'\n<b>╭ Total Files: </b>{folders}'
             if typ != 0:
                 msg += f'\n<b>├ Corrupted Files: </b>{typ}'
-            msg += f'\n<b>cc: </b>{self.tag}\n\n'
+            msg += f'\n<b>╰ #cc: </b>{self.tag}\n\n'
             if not files:
                 sendMessage(msg, self.bot, self.message)
             else:
